2fc67e29ad57c01daf758dd83759163ce6313d7f
Corey Hinshaw <hinshaw.25@osu.edu>
2012-12-11 00:37:55 -0500

    Moved configuration to Hiera.

--- nodes.pp
+++ nodes.pp
@@ -1,15 +1,6 @@
 # The basic set of classes that will apply to most nodes
 node basenode {
   include base
-
-  Network::If::Static {
-    netmask => '255.255.255.0',
-    gateway => '192.168.56.1',
-    dns1    => '128.146.1.7',
-    dns2    => '128.146.48.7',
-    domain  => 'eng-web.local',
-    ensure  => 'up',
-  }
 }
 
 # The configuration applied by default
@@ -17,9 +8,4 @@ node default inherits basenode {}
 
 node 'control.eng-web.local' inherits basenode {
   include puppetmaster
-
-  network::if::static { 'eth0':
-    ipaddress  => '192.168.56.51',
-    macaddress => $macaddress_eth0,
-  }
 }
--- templates.pp
+++ templates.pp
@@ -5,46 +5,14 @@ class base {
   include general
   include mail
   include firewall_rules
-
-  Network::If::Static {
-    netmask      => '255.255.255.0',
-    gateway      => '164.107.58.1',
-    dns1         => '128.146.1.7',
-    dns2         => '128.146.48.7',
-    domain       => 'eng-web.local',
-    ensure       => 'up',
-  }
-  class { 'resolv':
-    dns => [
-      '128.146.1.7',
-      '128.146.48.7'
-    ],
-    domain => 'eng-web.local',
-    search => 'eng-web.local',
-  }
-
-  class { 'ntp':
-    servers => [
-      'ntp1.service.ohio-state.edu',
-      'ntp2.service.ohio-state.edu',
-      '0.us.pool.ntp.org',
-      '1.us.pool.ntp.org'
-    ],
-  }
-
-  class { 'puppet': 
-    puppetserver => 'control.eng-web.local',
-  }
-
-  class { 'motd': 
-    message => 'Welcome to the College of Engineering web infrastructure!',
-  }
-
-  class { 'ssh': }
+  include puppet
+  include ntp
+  include motd
+  include network_settings
+  include ssh
 }
 
 # Node role definitions
 class puppetmaster {
   include puppet::master
 }
-

c11b1699c7c1a3106779db7d7cf996b1934e9ea8
little.129@osu.edu <little.129@osu.edu>
2013-10-14 12:35:42 -0400

    logstash replaces rsyslog

--- profiles.pp
+++ profiles.pp
@@ -93,14 +93,15 @@ class logserver {
     jarfile  => 'puppet:///modules/logstash/bin/logstash-current.jar',
     installpath => '/var/logstash'}
 
-  class { 'rsyslog::server':
-    enable_tcp                => true,
-    enable_udp                => true,
-    enable_onefile            => false,
-    server_dir                => '/var/log/aggregated/',
-    custom_config             => undef,
-    high_precision_timestamps => false,
-  }  
+  # Use logstash as rsyslog server
+  # class { 'rsyslog::server':
+  #  enable_tcp                => true,
+  #  enable_udp                => true,
+  #  enable_onefile            => false,
+  #  server_dir                => '/var/log/aggregated/',
+  #  custom_config             => undef,
+  #  high_precision_timestamps => false,
+  # }  
 
   firewall::rule { 'allow-rsyslog-server':
     weight => '375',

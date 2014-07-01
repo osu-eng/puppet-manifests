75860f312ae319bbf6bb22037997d20129cca99e
Brian Kildow <bkildow@gmail.com>
2014-07-01 14:32:22 -0400

    Adjusted version

--- profiles.pp
+++ profiles.pp
@@ -107,7 +107,7 @@ class logserver {
   class { 'logstash':
     manage_repo  => true,
     repo_version => '1.4',
-    version => '1.4.2-1',
+    version => '1.4.2-1_2c0f5a1',
     init_defaults => $config_hash
   }
 

6f785d55467b4ed6a4f588fdf251a79e3a43bb6d
Brian Kildow <bkildow@gmail.com>
2014-07-03 10:11:30 -0400

    Refactored elk stack into a module

--- profiles.pp
+++ profiles.pp
@@ -88,30 +88,7 @@ class webserver {
 
 class logserver {
 
-  include apache
-
-  vcsrepo { "/etc/logstash/conf.d":
-    ensure => latest,
-    provider => git,
-    source => "https://github.com/osu-eng/logstash-config.git",
-    revision => 'master'
-  }
-
-  class { 'elasticsearch':
-    manage_repo  => true,
-    repo_version => '1.2',
-    version => '1.2.1-1',
-    java_install => true
-  }
-
-  elasticsearch::instance { 'es-01': }
-
-  class { 'logstash':
-    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-1.4.2-1_2c0f5a1.noarch.rpm',
-    install_contrib => true,
-    contrib_package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/centos/logstash-contrib-1.4.2-1_efd53ef.noarch.rpm'
-  }
-
+  include elk
 
   #user { 'logstash-user':
   #  ensure   => present,
